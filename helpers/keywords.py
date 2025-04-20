108ad0ef8c890c1ffa391e198667a5a2c37d51d6
Rodrigo R. Marinho <marinho.rodrigo@gmail.com>
2025-04-20 09:14:54 -0300

    Create keywords.py

---
+++ helpers/keywords.py
@@ -0,0 +1,31 @@
+# helpers/keywords.py
+
+# Lista completa de palavras-chave a serem usadas nas buscas e análise dos editais
+PALAVRAS_CHAVE = [
+    "avental cirúrgico",
+    "campo",
+    "campo cirúrgico",
+    "campo cirúrgico estéril",
+    "campo estéril",
+    "kit cirúrgico em sms",
+    "folha para esterilização",
+    "sms para esterilização",
+    "papel crepado para esterilização",
+    "kit cirúrgico",
+    "papel grau cirúrgico",
+    "embalagem para esterilização",
+    "grau cirúrgico para esterilização",
+    "esterilização v2h2",
+    "indicador químico",
+    "indicador biológico",
+    "teste desafio",
+    "bowie & dick",
+    "bobina tyvek",
+    "bobina tyvek para esterilização",
+    "embalagem para esterilização por formaldeído",
+    "embalagem para esterilização por peróxido de hidrogênio",
+    "avental opa",
+    "campo hemodinâmica",
+    "campo parto normal",
+    "kit cirúrgico universal"
+]
